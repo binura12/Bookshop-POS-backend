52c6bbe37b0736441c458c21083fc4a8cc9cc08a
B1NUR4 <binuraofficial32@gmail.com>
2024-11-24 18:34:59 +0530

    removed some not used imports

--- src/main/java/edu/icet/config/EmailConfig.java
+++ src/main/java/edu/icet/config/EmailConfig.java
@@ -1,10 +1,10 @@
 package edu.icet.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Properties;
 
